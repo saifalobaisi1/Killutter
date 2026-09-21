817a28c5f6dfe15ed95ec05cbbd8c445835c7b4c
Saifalobaisi <saif.alobaisi@accrepro.com>
2026-09-22 00:45:23 +0300

    feat: watcher detects new files in Downloads and shows a message

--- Killutter/Modules/DownloadsOrganizer/Watcher.cs
+++ Killutter/Modules/DownloadsOrganizer/Watcher.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Killutter.Modules.DownloadsOrganizer
+﻿namespace Killutter.Modules.DownloadsOrganizer
 {
     internal class Watcher
     {
+        private string root;
+        private string downloads;
+        private FileSystemWatcher watcher;
+
+        public Watcher()
+        {
+            root = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            downloads = Path.Combine(root, "Downloads");
+            watcher = new FileSystemWatcher(downloads);
+
+            watcher.Created += OnCreated;
+
+            watcher.EnableRaisingEvents = false;
+        }
+
+        public void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            MessageBox.Show(value);
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        
+
+
     }
 }
