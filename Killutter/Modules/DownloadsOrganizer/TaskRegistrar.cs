a985f0fcace5ea444caa5c40006a663727210712
Saifalobaisi <saif.alobaisi@accrepro.com>
2026-09-24 17:42:54 +0300

    feat: Created the skeleton for TaskRegistrar

---
+++ Killutter/Modules/DownloadsOrganizer/TaskRegistrar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Killutter.Modules.DownloadsOrganizer
+{
+    internal static class TaskRegistrar
+    {
+        public static bool IsRegistered()
+        {
+            return true;
+        }
+
+        public static void Register()
+        {
+            
+        }
+
+        public static void Unregister()
+        {
+
+        }
+    }
+}
