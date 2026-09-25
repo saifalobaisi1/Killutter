2bde34073fa48cc66d771345669b79f755142989
Saifalobaisi <saif.alobaisi@accrepro.com>
2026-09-25 04:01:12 +0300

    feat: Created Register method for TaskRegistrar

--- Killutter/Modules/DownloadsOrganizer/TaskRegistrar.cs
+++ Killutter/Modules/DownloadsOrganizer/TaskRegistrar.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
 using System.Collections.Generic;
+using System.Security.Principal;
 using System.Text;
 
 namespace Killutter.Modules.DownloadsOrganizer
@@ -8,12 +10,41 @@ namespace Killutter.Modules.DownloadsOrganizer
     {
         public static bool IsRegistered()
         {
-            return true;
+            string taskName = "Killutter";
+
+            using (TaskService ts = new TaskService())
+            {
+                Task existingTask = ts.GetTask($@"\{taskName}");
+
+                if (existingTask != null)
+                {
+                    Console.WriteLine($"[FOUND] Task '{taskName}' is registered.");
+                    Console.WriteLine($"Status: {existingTask.State}");
+                    Console.WriteLine($"Last Run: {existingTask.LastRunTime}");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"[NOT FOUND] Task '{taskName}' does not exist.");
+                    return false;
+                }
+            }
         }
 
         public static void Register()
         {
-            
+            TaskDefinition td = TaskService.Instance.NewTask();
+            td.RegistrationInfo.Description = "Starts Killutter";
+
+            LogonTrigger lt = new LogonTrigger();
+            lt.Delay = TimeSpan.FromMinutes(1);
+            lt.UserId = WindowsIdentity.GetCurrent().Name;
+
+            td.Triggers.Add(lt);
+            td.Settings.ExecutionTimeLimit = TimeSpan.Zero;
+            td.Actions.Add(Environment.ProcessPath, "c:\\test.log");
+
+            TaskService.Instance.RootFolder.RegisterTaskDefinition("Killutter", td);
         }
 
         public static void Unregister()
