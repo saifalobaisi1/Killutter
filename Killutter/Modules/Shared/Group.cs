5bcc477fa646dbaa8856d24c8478dabf4946e101
Saifalobaisi <saif.alobaisi@accrepro.com>
2026-09-26 01:30:35 +0300

    feat: add Group record (Name, DestPath, RecognizedTypes) in Shared

---
+++ Killutter/Modules/Shared/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace Killutter.Modules.Shared
+{
+    public record Group
+    {
+        public string Name { get; set; }
+        public string DestPath { get; set; }
+        public List<string> RecognizedTypes { get; set; }
+    }
+}
