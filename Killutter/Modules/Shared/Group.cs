cb3625177f4baefcfab259b854783f4389c60c34
Saifalobaisi <saif.alobaisi@accrepro.com>
2026-09-26 12:52:12 +0300

    update: update Group to add unique Id field

--- Killutter/Modules/Shared/Group.cs
+++ Killutter/Modules/Shared/Group.cs
@@ -2,6 +2,7 @@
 {
     public record Group
     {
+        public Guid Id { get; }
         public string Name { get; set; }
         public string DestPath { get; set; }
         public List<string> RecognizedTypes { get; set; }
